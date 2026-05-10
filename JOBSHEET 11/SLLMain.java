9021176783aab523fedb80904e5befbe13d815d9
vesakhaa <anandavesakha2905@gmail.com>
2026-05-10 09:58:12 +0700

    SECOND COMMIT (Modif)

--- JOBSHEET 11/SLLMain.java
+++ JOBSHEET 11/SLLMain.java
@@ -1,20 +1,26 @@
+import java.util.Scanner;
 public class SLLMain {
    public static void main(String[] args) {
     SIngleLinkedLis04 sll = new SIngleLinkedLis04();
-        
-        // Membuat empat object mahasiswa (urutan parameter: nim, nama, kelas, ipk)
-         Mhs04 mhs1 = new Mhs04("Alvaro", "24212200", "1A", 4.0);
-         Mhs04 mhs2 = new Mhs04("Bimon", "23212201", "2B", 3.8);
-         Mhs04 mhs3 = new Mhs04("Cintia", "22212202", "3C", 3.5);
-         Mhs04 mhs4 = new Mhs04("Dirga", "21212203", "40", 3.6);
+        Scanner sc = new Scanner(System.in);
 
+        System.out.println("=== Tambah Data Mahasiswa ===");
+        System.out.print("Masukkan Nama  : ");
+        String nama = sc.nextLine();
+        System.out.print("Masukkan NIM   : ");
+        String nim = sc.nextLine();
+        System.out.print("Masukkan Kelas : ");
+        String kelas = sc.nextLine();
+        System.out.print("Masukkan IPK   : ");
+        double ipk = sc.nextDouble();
+
+        Mhs04 mhsInput = new Mhs04(nama, nim, kelas, ipk);
+        
+        sll.addFirst(mhsInput);
+        
+        System.out.println("\nMenampilkan Data:");
         sll.print();
-        sll.addFirst(mhs4);
-        sll.print();
-        sll.addLast(mhs1);
-        sll.print();
-        sll.insertAfter("Dirga", mhs3);
-        sll.insertAt(2, mhs2);
-        sll.print();
+        
+        sc.close();
    }
 }
