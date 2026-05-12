987323b58c9a711ac722c2670fc939e35483d11a
vesakhaa <anandavesakha2905@gmail.com>
2026-05-12 19:31:44 +0700

    comment ajja

--- JOBSHEET 11/Node04.java
+++ JOBSHEET 11/Node04.java
@@ -1,11 +1,11 @@
-import org.w3c.dom.Node;
+// import org.w3c.dom.Node;
 
-public class Node04 {
-    Mhs04 data;
-    Node04 next;
+// public class Node04 {
+//     Mhs04 data;
+//     Node04 next;
 
-    public Node04(Mhs04 data, Node04 next) {
-        this.data = data;
-        this.next = next;
-    }
-}
+//     public Node04(Mhs04 data, Node04 next) {
+//         this.data = data;
+//         this.next = next;
+//     }
+// }
